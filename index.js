65a6457a9f7f2e3742cf18df927de990b14227ec
talryan <nataliamryan@gmail.com>
2021-02-08 22:26:50 -0800

    commit tree ef70fbf79380f01f5f23dbdf919ac3a2d30280eb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map (source, callBack){
+let x = []
+for (let i = 0; i < source.length; i++ ) {
+    let theElement = source[i]
+    x.push(callBack(theElement))
+}
+return x
+}
+
+function reduce (source, callBack, startingPoint) {
+    let x = (!!startingPoint) ? startingPoint : source[0]
+    let i = (!!startingPoint) ? 0:1
+    
+    for (; i < source.length; i++ ) {
+        x = callBack(source[i], x)
+    }
+    return x
+
+}
